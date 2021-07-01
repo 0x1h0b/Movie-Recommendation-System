83fe3c6e4bd968fedf012ff5b06a8e0f5f75e956
Himanshu Bag <himanshubag12@gmail.com>
2021-07-01 23:40:30 +0530

    Basic functionality done

--- suggest.py
+++ suggest.py
@@ -1,21 +1,15 @@
 '''
 
-    This contains code for predicting simillar movies given the  movie name.
+    This contains code for predicting simillar movies by querying the csv data files
+    created and stored at ./data/ .
 
     TO DO :
-      - write a main function to deal with main values
       - request lib to fetch additional movie details from imdb website
     
 '''
-import argparse
+
 from numpy.testing._private.utils import break_cycles
 import pandas as pd
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-m','--movie',help='movie name',type=str)
-
-args = parser.parse_args()
-movie_name = args.movie
 
 
 def read_csv_files():
@@ -25,26 +19,30 @@ def read_csv_files():
         cosine_list1 = pd.read_csv('./data/top_50_cosine_values_list1.csv')
         cosine_list2 = pd.read_csv('./data/top_50_cosine_values_list2.csv')
 
-        return cosine_list1,cosine_list2,id_list1,id_list2,True
+        return cosine_list1,cosine_list2,id_list1,id_list2
     except FileNotFoundError:
-        print(' Csv Files not found !!')
-        return {},{},{},{},False
+        print('Error in read_csv_files function: '+'Csv Files not found !!')
+        return {},{},{},{}
     except Exception as e:
-        print(str(e))
-        return {},{},{},{},False
+        print('Error in read_csv_files function: '+str(e))
+        return {},{},{},{}
 
 
 def id_to_title(m_id,id_list1,id_list2):
     try:
         title1 = id_list1[id_list1['movie_id']==m_id]
         title2 = id_list2[id_list2['movie_id']==m_id]
+        imdb_id,title='',''
         if not title1.empty:
-            return title1['title'].to_dict()
+            title1_dict = list(title1.to_dict('index').values())[0]
+            imdb_id,title = title1_dict['imdb_id'],title1_dict['title']
         elif not title2.empty:
-            return title2['title'].to_dict()
-        return ''
+            title2_dict = list(title2.to_dict('index').values())[0]
+            imdb_id,title = title2_dict['imdb_id'],title2_dict['title']
+        return imdb_id,title
     except Exception as e:
-        print(str(e))
+        print('Error in id_2_title function: '+str(e))
+        return '',''
 
 
 
@@ -55,50 +53,54 @@ def title_to_id(name,id_list1,id_list2):
         movie_id,imdb_id,title='','',''
         flag=False
         if not id1.empty:
-            movie_id,imdb_id,title,flag = id1['movie_id'][0],id1['imdb_id'][0],id1['title'][0],True
+            movie_id,imdb_id,flag = id1['movie_id'][0],id1['imdb_id'][0],True
         elif not id2.empty:
-            movie_id,imdb_id,title,flag = id2['movie_id'][0],id2['imdb_id'][0],id2['title'][0],True
+            movie_id,imdb_id,flag = id2['movie_id'][0],id2['imdb_id'][0],True
     
-        return movie_id,imdb_id,title,flag
+        return movie_id,imdb_id,flag
     except Exception as e:
-        print(str(e))
-        return '','','',False
+        print('Error in title_2_id function: '+str(e))
+        return '','',False
 
 def get_cosine_matrix(id,cosine_list1,cosine_list2):
     try:
         val1 = cosine_list1[cosine_list1['movie_id']==id]
         val2 = cosine_list2[cosine_list2['movie_id']==id]
         l={}
-        flag=False
         if not val1.empty:
             l = val1.to_dict('index')
-            flag=True
         elif not val2.empty:
             l = val2.to_dict('index')
-            flag=True
-        return l[0],flag
+        return l[0]
     except Exception as e:
-        print(str(e))
-        return {},False
+        print('Error in get_cosine_matrix function: '+str(e))
+        return {}
+
+
+def main():
+    try:
+        movie_name = str(input('Enter your movies : '))
+        cosine_list1,cosine_list2,id_list1,id_list2= read_csv_files()
+        movie_id,imdb_id,id_found = title_to_id(movie_name,id_list1,id_list2)
+        if not id_found:
+            print('Movie not found in our DB !!')
+            return
+        movie_mat= get_cosine_matrix(movie_id,cosine_list1,cosine_list2)
+        result = []
+        for idx in range(1,50):
+            temp = movie_mat[str(idx)].replace('(','').replace(')','').split(',')
+            m_score,m_id = round(float(temp[1].strip())*100,2),int(temp[0])
+            temp = [round(float(j.strip()),2) for j in temp]
+            m_imdbid,m_name = id_to_title(m_id,id_list1,id_list2)
+            result.append([m_name,m_score,m_imdbid])
+        for m in result:
+            print(m[0],m[1],m[2])
+    except Exception as e:
+        print('Error in Main funtion: '+str(e))
+        return
 
 
 
-cosine_list1,cosine_list2,id_list1,id_list2,file_found = read_csv_files()
 
-movie_id,imdb_id,title,id_found = title_to_id(movie_name,id_list1,id_list2)
-print(id_found)
-print('Movie Name : ',title)
-cosine_matrix,matrix_found = get_cosine_matrix(movie_id,cosine_list1,cosine_list2)
-#print(cosine_matrix)
-result=[]
-for idx in range(1,50):
-    temp = cosine_matrix[str(idx)]
-    #print('1st: ',temp)
-    temp = temp.replace('(','').replace(')','').split(',')
-    #print('2nd: ',temp)
-    temp = [round(float(j.strip()),2) for j in temp]
-    m_name = id_to_title(int(temp[0]),id_list1,id_list2)
-    result.append([list(m_name.values())[0],temp[1]])
 
-for i in result[:20]:
-    print(i[0])
+main()
